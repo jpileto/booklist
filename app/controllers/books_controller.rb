c02fd19c59d72e457c4b89bcaba891edd54669be
jpileto <jp.ileto@icloud.com>
2017-04-26 15:11:22 -0400

    Add a CSV response

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,15 +2,22 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
-
     respond_to do |format|
         format.html
         format.text
+        format.csv 
+    #     format.json do
+    # formatted_data = @books.map do |books|
+    #   {
+    #     book_title: book.title,
+    #     location: book.author,
+    #     status: book.already_read
+    #   }
+    # end
+    #
+    # render json: formatted_data
+
     end
 
   end
-
-
-
-
 end
